d8f4c0443fd37bc9c7dca037be2257610fce6a10
t kam <t.kam@f5.com>
2019-10-18 20:51:34 +0900

    add missing allowed_networks var in main.tf

--- terraform-ansible-google/NGINX_F5_CDN/terraform/main.tf
+++ terraform-ansible-google/NGINX_F5_CDN/terraform/main.tf
@@ -19,10 +19,11 @@ module "cdn_network" {
   # When using these modules in your own templates, you will need to use a Git URL with a ref attribute that pins you
   # to a specific version of the modules, such as the following example:
   # source = "github.com/gruntwork-io/terraform-google-network.git//modules/vpc-network?ref=v0.1.2"
-  source      = "../../terraform_modules/gcp_vpc_network"
-  name_prefix = var.name_prefix
-  project     = var.project
-  region      = var.region
+  source           = "../../terraform_modules/gcp_vpc_network"
+  name_prefix      = var.name_prefix
+  project          = var.project
+  region           = var.region
+  allowed_networks = var.allowed_networks
 }
 
 # -------------------------
